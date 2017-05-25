d7bc0a429e2c6a16c830ff907101ab96c4a0561c
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-05-25 12:14:36 +0100

    setting up callbacks for text input components

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-export default class TextInut extends React.Component {
-  constructor(props) {
-    super(props);
-    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  getInitialState: function() {
+    return {value: this.props.text};
+  },
+  _handleKeyDown: function(e) {
+    switch (e.key) {
+      case 'Enter':
+        return this.props.doneEditing(this.props.itemId, this.state.value);
+      case 'Escape':
+        return this.props.cancelEditing(this.props.itemId);
+    }
+  },
+  _handleOnBlur: function(e) {
+    return this.cancelEditing(this.props.itemId);
+  },
+  _handleOnChange: function(e) {
+    this.setState({value: e.target.value})
+  },
+  cancelEditing: function() {
+    this.setState({'value': this.props.text});
+    return this.props.cancelEditing(this.props.itemId);
+  },
+  render: function() {
+    return <input className="edit"
+                  autoFocus={true}
+                  value={this.state.value}
+                  onChange={this._handleOnChange}
+                  type="text"
+                  ref="itemInput"
+                  onKeyDown={this._handleKeyDown}
+                  onBlur={this._handleOnBlur}/>
   }
-  render() {
-    return <input className="edit" autoFocus={true} type="text" />
-  }
-};
+})
